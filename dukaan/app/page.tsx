1c9f9d2ad4ec3aff9f20bf14ced2415a6438f47f
arvind <achurox1121@gmail.com>
2025-04-01 22:53:09 +0530

    init

---
+++ dukaan/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-background font-[family-name:var(--font-geist-sans)] w-[85vw]">
+      
+      <div>
+        <h1 className="text-black"> HIIIIII </h1>
+      </div>
+      
+    </div>
+  );
+}
