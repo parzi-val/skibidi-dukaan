f70dd96ca3994db730247f32647b95213eb721c3
arvind <achurox1121@gmail.com>
2025-04-23 21:29:44 +0530

    add forget pass page

---
+++ dukaan/app/forget-password/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'next/image'
+function ForgotPass
+() {
+  return (
+    <div className='min-h-[70vh]'>
+                 <Image 
+                alt="cat"
+                src="/catstare.jpg"
+                height={2000}
+                width={2000}
+                className='w-fit'
+                />
+    </div>
+  )
+}
+
+export default ForgotPass
